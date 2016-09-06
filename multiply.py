910f6db8422d3d610313c9639039a6a33fa9e9a8
David Trichard <david@adverline.com>
2016-09-06 11:24:09 +0200

    second test, modules and importation

---
+++ multiply.py
@@ -0,0 +1,15 @@
+# -*-coding:utf-8 -*
+"""module multipli contenant la fonction table"""
+
+def table(nb, max=10):
+	"""Fonction affichant la table de multiplication par nb de
+	1 * nb jusqu'a max * nb"""
+	i = 0
+	while i < max:
+		print(i + 1, "*", nb, "=", (i + 1) * nb)
+		i += 1
+
+# test de la fonction table
+if __name__ == "__main__":
+    table(4)
+    os.system("pause")
---
+++ test.py
@@ -0,0 +1,6 @@
+import os
+from multipli import *
+
+# test de la fonction table
+table(3, 20)
+os.system("pause")
